db340ca7a2ea7b3b908afc4bbf8c5b57185a7216
changhyun9 <ckdgus1713@naver.com>
2023-11-24 11:26:46 +0900

    ~ 2-07 기초 템플릿 사용법

---
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -0,0 +1,21 @@
+package com.example.sbb.question;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
